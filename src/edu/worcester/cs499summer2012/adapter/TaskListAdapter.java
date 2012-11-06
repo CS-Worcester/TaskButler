e315741ec12d641623e6db9f67cbe61fd5fa6e1e
Jonathan Hasenzahl <jon@jon-pc>
2012-11-05 19:09:08 -0500

    Change layout of task list in MainActivity
    * The display of tasks in MainActivity is now very close to how it will
      be in the final version
    * Some more tweaks to be done
    * Checkbox is not yet functional

--- src/edu/worcester/cs499summer2012/adapter/TaskListAdapter.java
+++ src/edu/worcester/cs499summer2012/adapter/TaskListAdapter.java
@@ -21,11 +21,16 @@ package edu.worcester.cs499summer2012.adapter;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+
 import android.app.Activity;
+import android.graphics.Color;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 import edu.worcester.cs499summer2012.R;
 import edu.worcester.cs499summer2012.comparator.TaskAutoComparator;
@@ -47,15 +52,13 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 	public static final int CUSTOM_SORT = 1;
 	
 	static class ViewHolder {
+		public CheckBox is_completed;
 		public TextView name;
-		public TextView is_completed;
-		public TextView priority;
-		public TextView category;
-		public TextView date_created;
-		public TextView date_modified;
-		public TextView date_due;
-		public TextView final_date_due;
-		public TextView notes;
+		public View category;
+		public ImageView priority;
+		public TextView due_date;
+		public ImageView alarm;
+		public ImageView recurrence;
 	}
 	
 	/**************************************************************************
@@ -105,28 +108,18 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 	@Override
 	public View getView(int position, View convert_view, ViewGroup parent) {
 		View row_view = convert_view;
-		if (row_view == null) {
+		if (row_view == null) {		
 			LayoutInflater inflater = activity.getLayoutInflater();
 			row_view = inflater.inflate(R.layout.row_task, null);
+			
 			ViewHolder view_holder = new ViewHolder();
-			view_holder.name = (TextView) 
-					row_view.findViewById(R.id.text_main_row_name);
-			view_holder.is_completed = (TextView)
-					row_view.findViewById(R.id.text_main_row_is_completed);
-			view_holder.priority = (TextView)
-					row_view.findViewById(R.id.text_main_row_priority);
-			view_holder.category = (TextView)
-					row_view.findViewById(R.id.text_main_row_category);
-			view_holder.date_created = (TextView)
-					row_view.findViewById(R.id.text_main_row_date_created);
-			view_holder.date_modified = (TextView)
-					row_view.findViewById(R.id.text_main_row_date_modified);
-			view_holder.date_due = (TextView)
-					row_view.findViewById(R.id.text_main_row_date_due);
-			view_holder.final_date_due = (TextView)
-					row_view.findViewById(R.id.text_main_row_final_date_due);
-			view_holder.notes = (TextView)
-					row_view.findViewById(R.id.text_main_row_notes);
+			view_holder.is_completed = (CheckBox) row_view.findViewById(R.id.checkbox_row_complete);
+			view_holder.name = (TextView) row_view.findViewById(R.id.text_row_name);
+			view_holder.category = (View) row_view.findViewById(R.id.view_row_category);
+			view_holder.priority = (ImageView) row_view.findViewById(R.id.image_row_priority);
+			view_holder.due_date = (TextView) row_view.findViewById(R.id.text_row_due_date);
+			view_holder.alarm = (ImageView) row_view.findViewById(R.id.image_row_alarm);
+			view_holder.recurrence = (ImageView) row_view.findViewById(R.id.image_row_recurrence);
 			
 			row_view.setTag(view_holder);
 		}
@@ -134,38 +127,52 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 		ViewHolder holder = (ViewHolder) row_view.getTag();
 		Task task = tasks.get(position);
 		
+		// Set is completed
+		if (task.isCompleted())
+			holder.is_completed.setChecked(true);
+		else
+			holder.is_completed.setChecked(false);
+		
 		// Set name
 		holder.name.setText(task.getName());
 		
-		// Set is completed
-		holder.is_completed.setText(task.isCompleted() ? "Finished" : 
-			"Unfinished");
+		// Set category
+		// TODO: Implement this
+		holder.category.setVisibility(View.VISIBLE);
+		holder.category.setBackgroundColor(Color.parseColor("#AA66CC"));
 		
 		// Set priority
-		holder.priority.setText(Task.LABELS[task.getPriority()]);
+		switch (task.getPriority()) {
+		case Task.URGENT:
+			holder.priority.setImageResource(R.drawable.ic_urgent);
+			break;
+		case Task.TRIVIAL:
+			holder.priority.setImageResource(R.drawable.ic_trivial);
+			break;
+		case Task.NORMAL:
+		default:
+			holder.priority.setImageResource(R.drawable.ic_normal);
+			break;
+		}
 		
-		// Set category
-		holder.category.setText("Category: " + task.getCategory());
+		// Set due date
+		if (task.hasDateDue())
+			holder.due_date.setText(DateFormat.format("'Due:' MM/dd/yy 'at' h:mm AA", task.getDateDueCal()));
 		
-		// Set date created
-		holder.date_created.setText("Created: " + task.getDateCreatedCal().getTime().toString());
+		// Set alarm
+		if (task.hasFinalDateDue())
+			holder.alarm.setVisibility(View.VISIBLE);
+		else
+			holder.alarm.setVisibility(View.INVISIBLE);
 		
-		// Set date modified
-		holder.date_modified.setText("Modified: " + task.getDateModifiedCal().getTime().toString());
-		
-		// Set date due
-		holder.date_due.setText("Due: " + (task.getDateDueCal() == null 
-				? "N/A" : task.getDateDueCal().getTime().toString()));
-		
-		// Set final date due
-		holder.final_date_due.setText("Due: " + (task.getFinalDateDueCal() == null 
-				? "N/A" : task.getFinalDateDueCal().getTime().toString()));
-		
-		// Set notes
-		holder.notes.setText("Notes: " + task.getNotes());
+		// Set recurrence
+		if (task.isRepeating())
+			holder.recurrence.setVisibility(View.VISIBLE);
+		else
+			holder.recurrence.setVisibility(View.INVISIBLE);
 		
 		// Set styles
-		if (!tasks.get(position).isCompleted()) {
+		/*if (!tasks.get(position).isCompleted()) {
 			holder.name.setTextAppearance(getContext(), 
 					R.style.text_task_not_completed);
 			holder.is_completed.setTextAppearance(getContext(), 
@@ -175,7 +182,7 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
 					R.style.text_task_completed);
 			holder.is_completed.setTextAppearance(getContext(), 
 					R.style.text_task_completed);
-		}
+		}*/
 		
 		return row_view;
 	}
